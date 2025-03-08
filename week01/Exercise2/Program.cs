8a4ec4e8fd973a33fbca17dde82fd7b2cbb08adf
faftech3210 <franklinakor@gmain.com>
2025-03-08 17:39:08 +0100

    Exercise2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grad percentage ");
+        string valueFromUser = Console.ReadLine();
+        int grad = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if(grad >= 90)
+        {
+            letter = "A";
+        }
+        else if (grad >= 80)
+        {
+            letter = "B";
+        }
+        else if (grad >= 70)
+        {
+            letter = "c";
+        }
+        else if (grad >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grad is {letter}");
+
+        if(grad >= 70)
+        {
+            Console.WriteLine("You passed");
+        }
+        else
+        {
+            Console.WriteLine("You failed! study hard next term");
+        }
     }
 }
