1fb02ab0f2c2f3a9141aa4251094fdfc3876a404
faftech3210 <franklinakor@gmain.com>
2025-03-08 22:50:55 +0100

    Exercise4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -3,7 +3,48 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers  = new List<int>();
+        int userNum = -1;
+        while (userNum != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 to quit");
+            string userInput = Console.ReadLine();
+            userNum = int.Parse(userInput);
+
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+        }
+// Part 1: Compute the sum ********************************************** 
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
+// Part 2: to get the average number in list***************************** 
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+// Part 3: Find the max**************************************************
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
